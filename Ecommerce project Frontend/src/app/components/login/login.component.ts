2a333421c806fe4cba2140a592bd25627a397f96
Mohammed.Saeed111 <mohamedsaied4556@gmail.com>
2025-01-18 16:13:37 +0200

    Update login.component.ts

--- Ecommerce project Frontend/src/app/components/login/login.component.ts
+++ Ecommerce project Frontend/src/app/components/login/login.component.ts
@@ -20,21 +20,17 @@ export class LoginComponent {
     password: ['', [Validators.required]], 
   });
 
-  // دالة للتعامل مع عملية تسجيل الدخول
   login() {
     let value = this.loginForm.value;
     this.authService.login(value.email!, value.password!).subscribe(
       (result: any) => {
         console.log(result);
-        // تخزين الـ token وبيانات المستخدم في localStorage
         localStorage.setItem('token', result.token);
         localStorage.setItem('user', JSON.stringify(result.user));
-        // التوجيه إلى الصفحة الرئيسية بعد تسجيل الدخول
         this.router.navigateByUrl('/');
       },
       (error) => {
         console.error(error);
-        // إظهار رسالة خطأ عند فشل تسجيل الدخول
         alert("Invalid email or password!");
       }
     );
